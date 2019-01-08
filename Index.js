e1d90e08a4026f59ea7f98ae8ac6aacb5e0e6a82
A Wiley <42783142+AustinWiley@users.noreply.github.com>
2019-01-08 13:59:56 -0700

    comments added

--- Index.js
+++ Index.js
@@ -5,39 +5,37 @@ const words = ["lemongrab", "huntress", "marceline", "lsp", "bmo", "bubblegum", 
 let guessesLeft = 12;
 let random;
 let secretWord;
-let chickenNugget;
+let progressDisplay;
 
 
 
 const checkGame = () => {
-    let meatBall = secretWord.wordDisplay();
-    if (meatBall === chickenNugget) {
+    let displayWord = secretWord.wordDisplay();
+    //check if a change was made to display word to see if you guessed right.
+    if (displayWord === progressDisplay) {
         console.log("\x1b[31m"+"\nINCORRECT :( \n"+"\x1b[0m")
         guessesLeft--;
         console.log(guessesLeft + ' Tries left.')
-    } else if (meatBall !== chickenNugget) {
+    } else if (displayWord !== progressDisplay) {
 
         console.log("\x1b[32m"+'\nCORRECT!!!' + "\x1b[0m")
     };
-    chickenNugget = meatBall;
-
+    progressDisplay = displayWord;
+    //check any tries left, if so check if word is complete.
     if (guessesLeft === 0) {
         console.log('You Failed, NEW WORD');
         gameReset();
-    } else if (meatBall === random) {
-        console.log(meatBall.capitalize());
+    } else if (displayWord === random) {
+        console.log(displayWord.capitalize());
         console.log('You Got it right! NEXT WORD')
         gameReset();
-    } else if (meatBall !== random) {
+    } else if (displayWord !== random) {
         playGame();
     };
 }
 
-
 const playGame = () => {
     console.log('\n' + secretWord.wordDisplay().capitalize() + '\n');
-    // console.log('\n------------------------------------------------------------')
-
     inquirer.prompt([
     // Here we create a basic text prompt.
     {
@@ -50,15 +48,9 @@ const playGame = () => {
     // check results
     let userguess = res.userguess.toLowerCase();
     secretWord.guessCheck(userguess)
-
     checkGame();
   });
 };
-
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1)
-  };
-
 
 const gameReset = () => {
     // console.log('------------------------New Word----------------------------')
@@ -66,124 +58,15 @@ const gameReset = () => {
     random = words[Math.floor(Math.random() * words.length)];
     secretWord = new Word(random);
     secretWord.newWord(random);
-    chickenNugget = secretWord.wordDisplay();
+    progressDisplay = secretWord.wordDisplay();
     // console.log(secretWord.wordDisplay());
     // console.log('\n------------------------------------------------------------')
     playGame()
-}
+};
 
-console.log('New Word\n')
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1)
+  };
+
+console.log('----------------New Word--------------------\n')
 gameReset();
-
-
-
-
-
-
-
-// var winnerText = document.getElementById("winner-text")
-// var winsText = document.getElementById("wins-text");
-// var wordText = document.getElementById("current-word-text");
-// var guessesLeftText = document.getElementById("guesses-left-text");
-// var guessText = document.getElementById("guess-text");
-// var imageHolder = document.getElementById("the-image");
-// var cardText = document.getElementById("answer-text");
-
-
-
-// var game = {
-// wins: 0,
-// guessesLeft: 12,
-// yourGuesses: [""], //letters you already chose
-// words: ["lemongrab", "huntress", "marceline", "lsp", "bmo", "bubblegum", "prismo", "jake the dog", "simon", "ice  king", "finn  the  human","peppermint  buttler"],
-// secretWord: "", // this stores the computers randomly chosen word.
-// answerArray: [], // stores the computers randomly picked word turned into an array.
-
-// // method resets game =====================================================================
-// gameReset: function () { 
-//     console.log("reset game function");
-//     this.secretWord = [""];
-//     this.answerArray = [""];
-//     this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
-
-//     console.log(this.secretWord);
-
-//     // we turn the word picked by the computer into an array.
-//     for (var i = 0; i < this.secretWord.length; i++){
-//     this.answerArray[i] = "_";
-//     };
-
-//     for (j = 0; j < this.secretWord.length; j++) {
-//         if (this.secretWord[j] === " ") {
-//             this.answerArray[j] = " ";
-//         }
-//     }
-
-//     // reset docement
-//     wordText.textContent = this.answerArray.join("");
-//     this.guessesLeft = 12;
-//     this.yourGuesses = [""];
-//     guessesLeftText.textContent = game.guessesLeft;
-//     guessText.textContent = game.yourGuesses;
-//     // winnerText.textContent = "Good Luck!"
-// },
-
-// // method rewrites stats ===================================================================
-// reWriteStats: function() {
-//     wordText.textContent = game.answerArray.join("")
-//     guessText.textContent = game.yourGuesses;
-//     guessesLeftText.textContent = game.guessesLeft;
-//     winsText.textContent = this.wins;
-
-//     console.log("rewrite stats")
-// },
-
-// // method happens when you hit a key ========================================================
-// letterSelect: function(event) {
-//     var userPick = event.key;
-//     userPick = userPick.toLowerCase();
-
-//     // this comparesyour input with the word you are guessing and updates.
-//     for (j = 0; j < this.secretWord.length; j++) {
-//         if (this.secretWord[j] === userPick) {
-//             this.answerArray[j] = userPick;
-//         }
-//     }
-
-//     if (this.answerArray.includes(userPick) === false && this.yourGuesses.includes(userPick) === false) {
-//         this.guessesLeft--;
-//     }
-
-//     if (this.yourGuesses.includes(userPick) === false) {
-//         this.yourGuesses = this.yourGuesses + userPick;
-//     }
-
-//     this.reWriteStats();
-// },
-
-// endGame: function() {
-//     if (game.answerArray.join("") === game.secretWord) {
-//         winnerText.textContent = "!!! " + "YOU WIN" + " !!!";
-//         imageHolder.src = "assets/images/BMO.png";
-//         cardText.textContent = "Hi  " + this.secretWord + "!";
-//         this.wins++;
-//         this.reWriteStats();
-//         this.gameReset();
-//     };
-
-//     if (game.guessesLeft === 0) {
-//         winnerText.textContent = "!!! " + "YOU LOSE" + " !!!";
-//         imageHolder.src = "assets/images/Prismo.png";
-//         cardText.textContent = "Try Again";
-//         this.gameReset();
-//     };
-// }
-// };
-
-// // initialize game    
-// game.gameReset();
-
-// document.onkeyup = function(event) {
-// game.letterSelect(event);
-// game.endGame();
-// }
